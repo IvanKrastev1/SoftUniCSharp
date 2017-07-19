123a6f641464f0ae27e01a499e1b9341bdf64786
IvanKrastev1 <vancata1926@gmail.com>
2017-07-19 13:54:23 +0300

    Lab Done!

---
+++ SoftUniCSharpOOPAdv/LabInterfacesAndAbstraction/Cars/Seat.cs
@@ -0,0 +1,24 @@
+﻿public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+           this.Model = model;
+           this.Color = color;
+        }
+        public string Model { get; }
+        public string Color { get; }
+        public string Start()
+        {
+            return "Engine Start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
