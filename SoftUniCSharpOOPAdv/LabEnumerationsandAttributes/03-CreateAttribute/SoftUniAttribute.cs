d3973c19c3bdf9df859182ccabbfc1db47cf91b5
IvanKrastev1 <vancata1926@gmail.com>
2017-08-01 13:59:02 +0300

    Lab Enum DONE!

---
+++ SoftUniCSharpOOPAdv/LabEnumerationsandAttributes/03-CreateAttribute/SoftUniAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+    using System;
+    using System.Security.Permissions;
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,AllowMultiple = true)]
+public class SoftUniAttribute: Attribute
+    {
+        public SoftUniAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
