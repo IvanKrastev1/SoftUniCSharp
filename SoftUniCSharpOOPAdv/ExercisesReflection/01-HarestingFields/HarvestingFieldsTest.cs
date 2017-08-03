04b7258ab074f473c776bff0f1991bdf44b963c2
IvanKrastev1 <vancata1926@gmail.com>
2017-08-03 15:29:11 +0300

    1 and 2 Exercises From ExeReflection !

---
+++ SoftUniCSharpOOPAdv/ExercisesReflection/01-HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,33 @@
+﻿using _01HarestingFields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public class HarvestingFieldsTest
+{
+    public static void Main(string[] args)
+    {
+        Type harvestFieldsType = typeof(HarvestingFields);
+        FieldInfo[] harvestingFields = harvestFieldsType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        Dictionary<string, Func<FieldInfo[]>> accessModifireFilters = new Dictionary<string, Func<FieldInfo[]>>()
+        {
+            {"private" ,() => harvestingFields.Where(f => f.IsPrivate).ToArray()},
+            {"protected" ,() => harvestingFields.Where(f => f.IsFamily).ToArray()},
+            {"public" ,() => harvestingFields.Where(f => f.IsPublic).ToArray()},
+            {"all" ,() => harvestingFields.ToArray()}
+        };
+
+        string requestedAccMod;
+        while ((requestedAccMod = Console.ReadLine()) != "HARVEST")
+        {
+            accessModifireFilters[requestedAccMod]()
+                .Select(f =>
+                    $"{f.Attributes.ToString().ToLower()} " +
+                    $"{f.FieldType.Name}" +
+                    $" {f.Name}")
+                .ToList()
+                .ForEach(r => Console.WriteLine(r.Replace("family", "protected")));
+        }
+    }
+}
---
+++ SoftUniCSharpOOPAdv/ExercisesReflection/02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,32 @@
+﻿using _02BlackBoxInteger;
+using System;
+using System.Linq;
+using System.Reflection;
+
+public class BlackBoxIntegerTests
+{
+    public static void Main(string[] args)
+    {
+        Type blackBoxType = typeof(BlackBoxInt);
+        BlackBoxInt blackBox = (BlackBoxInt)Activator.CreateInstance(blackBoxType, true);
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] tokens = input.Split('_');
+            string methodName = tokens[0];
+            int value = int.Parse(tokens[1]);
+
+            blackBoxType.GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic)
+                .Invoke(blackBox, new object[] { value });
+
+            object innerStateValue = blackBoxType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                .First()
+                .GetValue(blackBox)
+                .ToString();
+
+            Console.WriteLine(innerStateValue);
+
+
+        }
+    }
+}
