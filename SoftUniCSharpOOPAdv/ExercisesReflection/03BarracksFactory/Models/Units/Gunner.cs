cbfd2b752893104925274f14b00d43770a371b38
IvanKrastev1 <vancata1926@gmail.com>
2017-08-03 17:39:21 +0300

    Added Ex3!

---
+++ SoftUniCSharpOOPAdv/ExercisesReflection/03BarracksFactory/Models/Units/Gunner.cs
@@ -0,0 +1,13 @@
+﻿namespace _03BarracksFactory.Models.Units
+{
+    using Contracts;
+    public class Gunner :Unit
+    {
+        public const int Health = 20;
+        public const int AttackDamage = 20;
+
+        public Gunner() : base(Health, AttackDamage)
+        {
+        }
+    }
+}
---
+++ SoftUniCSharpOOPAdv/ExercisesReflection/03BarracksFactory/Models/Units/Horseman .cs
@@ -0,0 +1,12 @@
+﻿namespace _03BarracksFactory.Models.Units
+{
+    public class Horseman : Unit
+    {
+        public const int Health = 50;
+        public const int AttackDamage = 10;
+
+        public Horseman() : base(Health, AttackDamage)
+        {
+        }
+    }
+}
